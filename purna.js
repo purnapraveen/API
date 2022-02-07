18f2fd01c4efdfaa3f8a396bbf914cb0869dc57c
purnapraveen <99167328+purnapraveen@users.noreply.github.com>
2022-02-07 13:24:22 +0530

    Add files via upload

---
+++ purna.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const  cors = require("cors");
+const  bodyParser = require("body-parser");
+const  port = process.env.PORT || 8080;
+
+/*express config*/
+const app = express();
+app.use(cors({ origin: "*" }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+/*routes*/
+const router = express.Router();
+
+const usersRouter = require("./routes/users");
+app.get("/", (req, res) => {
+  res.send({message:"Welcome to REST API"});
+});
+
+app.use("/api/users/", usersRouter);
+module.exports = router ;
+
+/*server*/
+app.listen(port, () => {
+  console.log("Server is running on port: " + port);
+});
